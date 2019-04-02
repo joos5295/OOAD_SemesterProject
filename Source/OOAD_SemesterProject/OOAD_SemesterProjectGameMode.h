6b5da9e561921321232d514f93ac17a8d783f542
JohnO <joos5295@colorado.edu>
2019-04-01 17:44:51 -0700

    update

---
+++ Source/OOAD_SemesterProject/OOAD_SemesterProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OOAD_SemesterProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AOOAD_SemesterProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AOOAD_SemesterProjectGameMode();
+};
+
+
+
