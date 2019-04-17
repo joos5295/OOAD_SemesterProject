1f441d3c57c0006fedd5702d5c636e501d44beab
JoelCourtney <joel.courtney@colorado.edu>
2019-04-16 19:22:10 -0600

    Merge branch 'master' of /Users/joelcourtney/repos/OOAD/OOAD_SemesterProject with conflicts.

---
+++ inc/Game/Terrain/Terrain.h
@@ -0,0 +1,17 @@
+//
+// Created by Joel Courtney on 2019-04-16.
+//
+
+#ifndef OOAD_SEMESTERPROJECT_TERRAIN_H
+#define OOAD_SEMESTERPROJECT_TERRAIN_H
+
+#include "UI/Cell.h"
+#include "Game/Player.h"
+
+class Terrain : public Cell {
+public:
+    virtual bool canEnter() const = 0;
+    virtual void enter(Player) const = 0;
+};
+
+#endif //OOAD_SEMESTERPROJECT_TERRAIN_H
