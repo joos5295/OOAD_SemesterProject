4ad119505cc852b6ec875028dea4933dc533c280
joos5295 <joos5295@colorado.edu>
2019-04-16 18:52:51 -0600

    Created Level

---
+++ inc/Game/Level.h
@@ -0,0 +1,24 @@
+//
+// Created by user on 4/16/19.
+//
+
+#ifndef OOAD_SEMESTERPROJECT_LEVEL_H
+#define OOAD_SEMESTERPROJECT_LEVEL_H
+
+#include "Entity.h"
+#include "Player.h"
+#include "Enemy.h"
+#include <vector>
+#include <iostream>
+
+class Level{
+private:
+    //TerrainMap terrainMap;
+    Player player;
+    std::vector<Enemy> EnemyList;
+    //std::vector<Item> ItemList;
+
+
+};
+
+#endif //OOAD_SEMESTERPROJECT_LEVEL_H
